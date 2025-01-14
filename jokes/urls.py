210685fa99eb59da7cfdfae88bdb063c1c2c8130
Sam Geminiano <158030364+DevSam20@users.noreply.github.com>
2025-01-14 15:49:47 +0800

    jokes

--- jokes/urls.py
+++ jokes/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import JokeListView
+from .views import (JokeCreateView, JokeDeleteView, JokeDetailView,
+                    JokeListView, JokeUpdateView)
+
 app_name = 'jokes'
 urlpatterns = [
+    path('joke/create/', JokeCreateView.as_view(), name='create'),
+    path('joke/<int:pk>/delete/', JokeDeleteView.as_view(), name='delete'),
+    path('joke/<int:pk>/', JokeDetailView.as_view(), name='detail'),
     path('', JokeListView.as_view(), name='list'),
+    path('joke/<int:pk>/update/', JokeUpdateView.as_view(), name='update'),
 ]
--- jokes/views.py
+++ jokes/views.py
@@ -1,6 +1,27 @@
-from django.views.generic import ListView
+from django.views.generic import (
+    CreateView, DeleteView, DetailView, ListView, UpdateView)
 from .models import Joke
+from django.urls import reverse_lazy
+
+
+class JokeCreateView(CreateView):
+    model = Joke
+    fields = ['question', 'answer']
+
+
+class JokeDeleteView(DeleteView):
+    model = Joke
+    success_url = reverse_lazy('jokes:list')
+
+
+class JokeDetailView(DetailView):
+    model = Joke
 
 
 class JokeListView(ListView):
     model = Joke
+
+
+class JokeUpdateView(UpdateView):
+    model = Joke
+    fields = ['question', 'answer']
