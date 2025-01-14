7418d029e76598c6115870da9e7921fc7f3e93de
Sam Geminiano <158030364+DevSam20@users.noreply.github.com>
2025-01-14 11:53:51 +0800

    view django

---
+++ pages/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.views.generic import TemplateView
+
+
+class HomePageView(TemplateView):
+    template_name = 'pages/home.html'
+
+
+class AboutUsView(TemplateView):
+    template_name = 'pages/about_us.html'
